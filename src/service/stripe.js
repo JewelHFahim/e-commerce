dbb28723c06db9a8aba7cce697042a35f3514ece
JewelHFahim <jewelhfahim@gmail.com>
2025-04-08 07:58:15 +0600

    updated routes

--- src/service/stripe.js
+++ src/service/stripe.js
@@ -1,10 +1,7 @@
 const Stripe = require("stripe");
 
-const stripe = new Stripe(
-  "sk_test_51M6RfEDZfW4raXLHO2Z1slgQ3533z1UvveBNB7BDsM1RwDafVdgqzTuN8DQPrLNGkblIre5mRA7fs4rvHhgBlnH500Ibu939nk",
-  {
-    apiVersion: "2022-11-15",
-  }
-);
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {
+  apiVersion: "2022-11-15",
+});
 
 module.exports = stripe;
