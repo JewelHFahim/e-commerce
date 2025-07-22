53512ca6051698a9775d9f227e69cec8552980b9
JewelHFahim <jewelhfahim@gmail.com>
2025-07-23 02:12:42 +0600

    user updated

--- app.js
+++ app.js
@@ -15,19 +15,19 @@ const app = express();
 // 🔹 Connect to MongoDB
 connectDB();
 
-// const allowedOrigins = process.env.ALLOWED_ORIGINS ? process.env.ALLOWED_ORIGINS?.split(",").map((origin) => origin.trim()) : [];
-const allowedOrigins = "http://localhost:3000";
+const allowedOrigins = process.env.ALLOWED_ORIGINS ? process.env.ALLOWED_ORIGINS?.split(",").map((origin) => origin.trim()) : [];
+// const allowedOrigins = "http://localhost:3000";
 
 // 🔹 Middleware
 app.use(
   cors({
-    // origin: function (origin, callback) {
-    //   if (!origin || allowedOrigins.includes(origin)) {
-    //     callback(null, true);
-    //   } else {
-    //     callback(new Error("Not allowed by CORS"));
-    //   }
-    // },
+    origin: function (origin, callback) {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
     origin: "*",
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
